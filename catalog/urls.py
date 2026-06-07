0898eb8d7b3c434cbad259e3df87170c9a689576
Наталья Шахтарина <natshakht@yandex.ru>
2026-06-07 23:29:12 +1400

    Третье ДЗ: детальная страница, цикл, наследование шаблонов, форма добавления товара, пагинация, обновлён README

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/create/', views.product_create, name='product_create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product, Contact
+from django.shortcuts import redirect
+from .forms import ProductForm
+from django.core.paginator import Paginator
 
 def home(request):
-    last_products = Product.objects.all().order_by('-created_at')[:5]
-    print("Последние 5 продуктов:")
-    for p in last_products:
-        print(f"{p.name} - {p.price}")
-    return render(request, 'catalog/home.html', {'last_products': last_products})
+    products = Product.objects.all()
+    paginator = Paginator(products, 3)  # 3 товара на страницу
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'catalog/home.html', {'page_obj': page_obj})
 
 def contacts(request):
     contact = Contact.objects.first()
@@ -18,3 +21,17 @@ def contacts(request):
         print(f'Имя: {name}, Телефон: {phone}, Сообщение: {message}')
         return HttpResponse('Спасибо, ваше сообщение отправлено!')
     return render(request, 'catalog/contacts.html', {'contact': contact})
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = ProductForm()
+    return render(request, 'catalog/product_form.html', {'form': form})
