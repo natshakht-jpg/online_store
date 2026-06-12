a096bd6f5c4a8b0dbf2c6c15b865a4abcaa06d83
Наталья Шахтарина <natshakht@yandex.ru>
2026-06-12 22:00:12 +1400

    Пятое ДЗ: полный CRUD, валидация формы, стилизация, проверка изображения, README

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,7 +1,78 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Product
+
+# Список запрещённых слов (все в нижнем регистре)
+FORBIDDEN_WORDS = [
+    'казино', 'криптовалюта', 'крипта', 'биржа',
+    'дешево', 'бесплатно', 'обман', 'полиция', 'радар'
+]
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'category', 'image']
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if not name:
+            return name
+        name_lower = name.lower()
+        for word in FORBIDDEN_WORDS:
+            if word in name_lower:
+                raise ValidationError(f'Название не должно содержать слово "{word}"')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if description:
+            desc_lower = description.lower()
+            for word in FORBIDDEN_WORDS:
+                if word in desc_lower:
+                    raise ValidationError(f'Описание не должно содержать слово "{word}"')
+        return description
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price < 0:
+            raise ValidationError('Цена не может быть отрицательной')
+        return price
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        if image:
+            # Проверка размера (не более 5 МБ)
+            if image.size > 5 * 1024 * 1024:
+                raise ValidationError('Размер изображения не должен превышать 5 МБ')
+
+            # Проверка формата
+            import os
+            ext = os.path.splitext(image.name)[1].lower()
+            if ext not in ['.jpg', '.jpeg', '.png']:
+                raise ValidationError('Разрешены только изображения форматов JPEG, JPG, PNG')
+        return image
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Стилизация всех полей
+        for field_name, field in self.fields.items():
+            # Если поле — чекбокс (например, булево поле)
+            if field.widget.__class__.__name__ == 'CheckboxInput':
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                # Все остальные поля получают класс form-control
+                field.widget.attrs['class'] = 'form-control'
+
+            # Плейсхолдеры
+            if field_name == 'name':
+                field.widget.attrs['placeholder'] = 'Введите название товара'
+            elif field_name == 'description':
+                field.widget.attrs['placeholder'] = 'Введите описание товара'
+            elif field_name == 'price':
+                field.widget.attrs['placeholder'] = 'Введите цену'
+            elif field_name == 'category':
+                field.widget.attrs['placeholder'] = 'Выберите категорию'
+            elif field_name == 'image':
+                field.widget.attrs['placeholder'] = 'Выберите изображение'
--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, UpdateView, DeleteView
 from .models import Product, Contact
 from .forms import ProductForm
+from django.core.paginator import Paginator
+from django.urls import reverse_lazy
 
 class ProductListView(ListView):
     model = Product
     template_name = 'catalog/home.html'
-    context_object_name = 'page_obj'
+    context_object_name = 'products'
     paginate_by = 3
 
-    def get_queryset(self):
-        return Product.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = self.get_queryset()
+        paginator = Paginator(products, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+        return context
 
 class ProductDetailView(DetailView):
     model = Product
@@ -33,16 +41,35 @@ class ContactView(TemplateView):
         print(f'Имя: {name}, Телефон: {phone}, Сообщение: {message}')
         return HttpResponse('Спасибо, ваше сообщение отправлено!')
 
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    return render(request, 'catalog/product_detail.html', {'product': product})
-
 def product_create(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('/')
+            # Дополнительная проверка запрещённых слов
+            name = form.cleaned_data.get('name')
+            description = form.cleaned_data.get('description')
+            forbidden = ['казино', 'криптовалют', 'крипт', 'бирж', 'дешев', 'бесплатн', 'обман', 'полиц', 'радар']
+            for word in forbidden:
+                if word in name.lower():
+                    form.add_error('name', f'Название не должно содержать слово "{word}"')
+                    has_error = True
+                if description and word in description.lower():
+                    form.add_error('description', f'Описание не должно содержать слово "{word}"')
+                    has_error = True
+            if not has_error:
+                form.save()
+                return redirect('/')
     else:
         form = ProductForm()
     return render(request, 'catalog/product_form.html', {'form': form})
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('home')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('home')
