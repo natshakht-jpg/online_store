7f1c6078fe82c40d134bfd2bb70710297a891c06
Наталья Шахтарина <natshakht@yandex.ru>
2026-06-28 03:48:09 +1400

    ДЗ7: система прав доступа (модератор, владелец, суперпользователь), поля is_published и owner, кастомные права, группа, исправлен выход, админка, README

--- catalog/views.py
+++ catalog/views.py
@@ -6,6 +6,7 @@ from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from .models import Product, Contact
 from .forms import ProductForm
+from django.http import HttpResponseForbidden
 
 class ProductListView(ListView):
     model = Product
@@ -31,7 +32,11 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
@@ -39,10 +44,22 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('home')
 
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.owner != request.user and not request.user.is_superuser:
+            return HttpResponseForbidden('Вы не владелец этого продукта')
+        return super().dispatch(request, *args, **kwargs)
+
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('catalog:home')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.owner != request.user and not request.user.is_superuser and not request.user.has_perm('catalog.can_unpublish_product'):
+            return HttpResponseForbidden('У вас нет прав на удаление этого продукта')
+        return super().dispatch(request, *args, **kwargs)
 
 class ContactView(TemplateView):
     template_name = 'catalog/contacts.html'
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'phone', 'country', 'is_staff')
-    fields = ('email', 'avatar', 'phone', 'country', 'is_active', 'is_staff', 'is_superuser')
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'phone', 'country', 'is_staff', 'is_moderator')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Личная информация', {'fields': ('avatar', 'phone', 'country')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    def is_moderator(self, obj):
+        return obj.groups.filter(name='Модератор продуктов').exists()
+    is_moderator.boolean = True
+    is_moderator.short_description = 'Модератор'
